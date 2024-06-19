8f40a9f360df5b00bae8aa838312536990b72d91
Anatolii Stoliarenko <anatolii.stoliarenko@gmail.com>
2024-06-20 00:12:28 +0200

    start taska componnet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-user';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  users = DUMMY_USERS
+  selectedUserName?: string
+
+  selectedUser(id:string){
+    let index = Number(id.slice(1))
+    this.selectedUserName = this.users[index-1].name
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-user'
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+import { Component, EventEmitter, Input, Output, computed, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,18 +7,30 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  // selectedUser = DUMMY_USERS[randomIndex];
-
-  imagePath = computed(() => 'users/' + this.selectedUser().avatar)
-  // get imagePath() {
-  //   return 'users/' + this.selectedUser.avatar
-  // }
+  @Input({required: true}) id!: string;  // id = input.required<string>()
+  @Input({required: true}) avatar!: string; // avatar = input.required<string>()
+  @Input({required: true}) name!: string;   // name = input.required<string>()
+  @Output() select = new EventEmitter<string>() //select = output<string>()
+  
+  
+  get imagePath() { // imagePath = computed(() => 'users/' + this.avatar())
+    return 'users/' + this.avatar
+  }
+  
 
   onSelectUser(){
-    // console.log('Clicked!')
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[randomIndex])
-    // this.selectedUser = DUMMY_USERS[randomIndex] without signal
+    this.select.emit(this.id)
   }
 }
+
+
+/*
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+ selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+      onSelectUser(){
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    this.selectedUser.set(DUMMY_USERS[randomIndex])
+  }
+*/
